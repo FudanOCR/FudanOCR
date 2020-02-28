553c3b5d336d0f2c1a8f07f34918234276c37c15
JinGyeSetBirdsFree <576194329@qq.com>
2020-02-28 12:06:22 +0000

    拆解utils部分

---
+++ utils/loadData.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import collections
+
+def loadData(v, data):
+    '''
+    将数据data搬运到v中
+
+    :param torch.Tensor v 目标张量
+    :param torch.Tensor data 源张量
+    '''
+    v.data.resize_(data.size()).copy_(data)
